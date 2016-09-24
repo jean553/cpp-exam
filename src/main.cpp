c38c2861ce234ab5ce859f26a451cabd58e952c2
jean <jean@bst>
2016-09-24 22:12:11 +0800

    close integrate loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,28 @@
 #include <string>
 #include <streambuf>
 
+/**
+ * @brief remove the quotes (") from given string
+ *
+ * @param str reference to the concerned string
+ *
+ * @throw std::bad_alloc memory error
+ * @throw std::terminate error occured in the called sub-functions
+ * algorithms
+ * @throw std::out_of_range error concerning the given positions
+ *
+ * str reference is not 'const', because we modify
+ * the referred string
+ */
+void removeDoubleQuotes(std::string& str)
+{
+    str.erase(std::remove(str.begin(), str.end(), '\"'), str.end());
+}
+
 int main()
 {
     constexpr char FILE_PATH[] {"data.json"};
+    constexpr char SEPARATOR[] {"-------------------------------------"};
 
     std::ifstream file(FILE_PATH);
 
@@ -30,6 +49,44 @@ int main()
     );
 
     nlohmann::json jsonData = nlohmann::json::parse(data);
+
+    std::string answer;
+    unsigned short mark {0};
+
+    for (auto& question : jsonData)
+    {
+        std::string strQuestion = question["question"];
+        std::string strCode = question["code"];
+        std::string strA = question["a"];
+        std::string strB = question["b"];
+        std::string strC = question["c"];
+        std::string strD = question["d"];
+
+        removeDoubleQuotes(strQuestion);
+        removeDoubleQuotes(strCode);
+        removeDoubleQuotes(strA);
+        removeDoubleQuotes(strB);
+        removeDoubleQuotes(strC);
+        removeDoubleQuotes(strD);
+
+        std::cout << strQuestion << std::endl << std::endl;
+        std::cout << SEPARATOR << std::endl;
+        std::cout << strCode << std::endl;
+        std::cout << SEPARATOR << std::endl << std::endl;
+        std::cout << "A: " << strA << std::endl;
+        std::cout << "B: " << strB << std::endl;
+        std::cout << "C: " << strC << std::endl;
+        std::cout << "D: " << strD << std::endl << std::endl;
+        std::cout << "Your answer: ";
+        std::cin >> answer;
+
+        if (answer == question["answer"])
+        {
+            mark++;
+        }
+    }
+
+    std::cout << "Mark: " << mark;
 
     return EXIT_SUCCESS;
 }
