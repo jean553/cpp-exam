1f977cf12203a93bd1cfc5413cf65291d81b2d4a
jean <jean@bst>
2016-11-28 12:11:54 +0800

    close integrate constexpr questions

--- src/main.cpp
+++ src/main.cpp
@@ -79,12 +79,13 @@ int main()
     std::string data;
 
     /* TODO: use the experimental method std::experimental::make_array() ? */
-    const std::array<std::string, 5> files = {
+    const std::array<std::string, 6> files = {
         "template_types_deduction_references.json",
         "template_types_deduction_constant_references.json",
         "template_types_deduction_pointers.json",
         "template_types_deduction_constant_pointers.json",
-        "template_types_deduction_universal_references.json"
+        "template_types_deduction_universal_references.json",
+        "constexpr.json"
     };
 
     loadAllFiles(
